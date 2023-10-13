4a13d19237e58f48b2aadd6bca27247a9740020f
IyaadDev <113124292+IyaadDev@users.noreply.github.com>
2023-10-13 20:47:50 +0400

    Launch A Test

--- app/src/index.js
+++ app/src/index.js
@@ -1,17 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import "./bootstrap.min.css"
+import "./global.css"
+
+function Greeting() {
+  var testVariable = "Hello World!";
+  console.log(testVariable);
+  console.log("If you see this, your React app is live!");
+  return (
+    <div className='text-center container life'>
+      <img className="logo" src="https://schooltool.io/assets/img/logo.webp?h=0722d3247654ad6b5121ce1ebd28d5fc" alt="Logo" />
+      <h1 className='heading'>If you see this, School Tool is live!</h1>
+      <div className='box text-center'>
+        <h2 className="jumbotron text-center">Heading Test</h2>
+        <h6 className="code text-center">{testVariable}</h6>
+      </div>
+    </div>
+  );
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Greeting />
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
