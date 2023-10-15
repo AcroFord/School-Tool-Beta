08f706a7a4e87a9e69e4ea30d9a4b472f0f14886
IyaadDev <113124292+IyaadDev@users.noreply.github.com>
2023-10-15 20:57:13 +0400

    Logoff

--- src/components/Widgets.js
+++ src/components/Widgets.js
@@ -3,45 +3,45 @@ import axios from 'axios';
 import TextPanel from './Panels';
 
 function Widgets() {
-    const [widgets, setListOfWidgets] = useState([]);
-  
-    useEffect(() => {
-      axios
-        .get('https://faas-blr1-8177d592.doserverless.co/api/v1/web/fn-3d00acfd-4377-4fe1-a494-f5d6925c1201/demo/widgets')
-        .then((response) => {
-          if (Array.isArray(response.data)) {
-            setListOfWidgets(response.data);
-          } else {
-            console.error('Response data is not an array:', response.data);
+  const [widgets, setListOfWidgets] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get('https://faas-blr1-8177d592.doserverless.co/api/v1/web/fn-3d00acfd-4377-4fe1-a494-f5d6925c1201/demo/widgets')
+      .then((response) => {
+        if (Array.isArray(response.data)) {
+          setListOfWidgets(response.data);
+        } else {
+          console.error('Response data is not an array:', response.data);
+        }
+      })
+      .catch((error) => {
+        console.error('Error fetching data:', error);
+      });
+  }, []);
+
+  console.log("Widgets:", widgets);
+  return (
+    <div>
+      {Array.isArray(widgets) ? (
+        widgets.map((widget, index) => {
+          if (widget.Type === 'TextPanel') {
+            return (
+              <TextPanel
+                key={index}
+                heading={widget.Content.heading}
+                content={widget.Content.content}
+                className={widget.size}
+              />
+            );
           }
+          return null;
         })
-        .catch((error) => {
-          console.error('Error fetching data:', error);
-        });
-    }, []);
-  
-    console.log("Widgets:", widgets);
-  
-    return (
-      <div>
-        {Array.isArray(widgets) ? (
-          widgets.map((widget, index) => {
-            if (widget.Type === 'TextPanel') {
-              return (
-                <TextPanel
-                  key={index}
-                  heading={widget.Content.heading}
-                  content={widget.Content.content}
-                />
-              );
-            }
-            return null;
-          })
-        ) : (
-          <p>No widgets to display.</p>
-        )}
-      </div>
-    );
-  }
+      ) : (
+        <p>No widgets to display.</p>
+      )}
+    </div>
+  );
+}
 
 export default Widgets;
