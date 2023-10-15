4d136d23f9fa9ef0e1e0723c692a20a4f85f27b0
IyaadDev <113124292+IyaadDev@users.noreply.github.com>
2023-10-15 19:23:51 +0400

    Cleaned imports, added bootstrap css map

--- src/index.js
+++ src/index.js
@@ -1,18 +1,18 @@
-import React, { useEffect, useState } from 'react';
 import ReactDOM from 'react-dom/client';
 import './bootstrap.min.css';
 import './global.css';
-import axios from 'axios';
 import Nav from './components/Nav';
-import TextPanel from './components/Panels';
-import Widgets from "./components/Widgets"
+import Widgets from "./components/Widgets";
+import "./widgets.css"
 
 function Main() {
   return (
     <div>
       <Nav />
       <div className="container">
-        <Widgets />
+        <div className='row'>
+          <Widgets className="panel" />
+        </div>
       </div>
     </div>
   );
